8f859c543d52f463b848326ab04f873b6d478b67
H <hrvojep@yahoo.com>
2023-04-04 11:27:21 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo Your container args are: "$1 " "$2" 
-consul agent -config-file=/consul/config/config-file.json $1 $2
+consul agent -config-file=/consul/config/config-file.json $1 $2 &
+nginx
